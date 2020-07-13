a9a8e0c31efb1f396fb60a5f21ab56e8efaf6259
David Sumner <davidphilipsumner@gmail.com>
2020-07-13 14:12:53 +0100

    Configure GULP file to have Babel and watch for JS.    Link script to the ES5 js in the dist files

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,16 @@
     const sass = require("gulp-sass");
     sass.compiler = require('sass');
     const autoprefixer = require("gulp-autoprefixer");
+    const babel = require('gulp-babel');
 
 
-
+    function babelJS() {
+        return gulp.src('src/js/app.js', {allowEmpty:true})
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('src/js/dist'))
+    }
 
 
     function scssCompile() {
@@ -28,9 +35,14 @@
 
 
     function watchFiles() {
-        console.log("\r\n Watching Scss files \r\n");
+        
         gulp.watch('src/**/*.scss', gulp.series(scssCompile, autoprefix));
+        console.log("\r\n Watching Scss files ");
+
+        gulp.watch('src/js/*.js', babelJS) ;
+        console.log(" Watching JS files \r\n");
     }
 
 exports.styles = gulp.series(scssCompile,autoprefix);
 exports.watch = watchFiles;
+exports.babel = babelJS
